8b4905745af2b5db4e99064baec0a2a534ee9826
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-07-27 18:41:24 +0200

    Update Panel for better handling Panel+Nav case

--- src/objects/Panel.js
+++ src/objects/Panel.js
@@ -1,55 +1,64 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import classnames from 'classnames'
 
 import s from 'blaze/scss/objects.panels.scss'
 
-const Panel = ({ topNav, bottomNav, children, ...rest }) => {
-  const className = classnames(
-    s['o-panel'], {
-      [s['o-panel--nav-top']]: topNav,
-      [s['o-panel--nav-bottom']]: bottomNav
-    }
-  )
-
-  let navTop = ''
-  let navBottom = ''
-  const content = []
-
-  if (children.length > 1) {
-    children.forEach((child, index) => {
-      if (child.isReactComponent && child.type.name === 'BlazeNav') {
-        if (index === 0) {
-          navTop = child
-        } else {
-          navBottom = child
-        }
-      } else {
-        content.push(child)
-      }
-    })
-  } else {
-    content.push(children)
+export default class Panel extends Component {
+  static propTypes = {
+    topNav: PropTypes.element,
+    bottomNav: PropTypes.element
   }
 
-  return (
-    <div className={s['o-panel-container']} {...rest}>
-      {navTop}
-      <div className={className}>
-        {content}
+  constructor (props) {
+    super(props)
+
+    this.getPanel = this.getPanel.bind(this)
+  }
+
+  componentDidMount () {
+    if (process.env.NODE_ENV !== 'production') {
+      if (this.panel.children.length > 1) {
+        [...this.panel.children].forEach((child) => {
+          if (child.indexOf('c-nav') === -1) {
+            return new Error(
+              'Invalid `topNav` or `bottomNav` prop supplied to `Panel`. Please only supply `Nav` component. Validation failed.'
+            )
+          }
+        })
+      }
+    }
+  }
+
+  getPanel (node) {
+    if (process.env.NODE_ENV !== 'production') {
+      if (node.children.length > 1) {
+        [...node.children].forEach((child) => {
+          if (child.indexOf('c-nav') === -1) {
+            console.warn('Invalid `topNav` or `bottomNav` prop supplied to `Panel`. Please only supply `Nav` component. ' +
+              'Validation failed.')
+          }
+        })
+      }
+    }
+  }
+
+  render () {
+    const { topNav = null, bottomNav = null, children, ...rest } = this.props
+    const className = classnames(
+      s['o-panel'], {
+        [s['o-panel--nav-top']]: topNav,
+        [s['o-panel--nav-bottom']]: bottomNav
+      }
+    )
+
+    return (
+      <div className={s['o-panel-container']} {...rest} ref={this.getPanel}>
+        {topNav}
+        <div className={className}>
+          {children}
+        </div>
+        {bottomNav}
       </div>
-      {navBottom}
-    </div>
-  )
+    )
+  }
 }
-
-Panel.propTypes = {
-  topNav: PropTypes.bool,
-  bottomNav: PropTypes.bool
-}
-
-Panel.defaultProps = {
-  topNav: false,
-  bottomNav: false
-}
-
-export default Panel
