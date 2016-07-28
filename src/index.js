6000523a0ab25f3e615e52eec30bc462daf03ec3
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-07-28 19:52:41 +0200

    Move index in src

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// Objects
+import Container from './objects/Container'
+import { Grid, Cell } from './objects/Grid'
+import Image from './objects/Image'
+import Panel from './objects/Panel'
+
+// Components
+import Address from './components/Address'
+import { AlertContainer, Alert } from './components/Alert'
+import Badge from './components/Badge'
+import Bubble from './components/Bubble'
+import { ButtonGroup, Button } from './components/Button'
+import { Card, CardContent, CardHeading, CardItem, CardDivider } from './components/Card'
+
+// Utilities
+import Align from './utilities/Align'
+
+// Named exports
+export {
+  Container,
+  Grid,
+  Cell,
+  Image,
+  Panel,
+  Address,
+  AlertContainer,
+  Alert,
+  Badge,
+  Bubble,
+  ButtonGroup,
+  Button,
+  Card,
+  CardContent,
+  CardHeading,
+  CardItem,
+  CardDivider,
+  Align
+}
