45b66fde389f3da9731cc5d44431dcacc9e8e6b5
Nicolas Goudry <goudry.nicolas@gmail.com>
2016-07-29 02:29:55 +0200

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
-var webpack = require('webpack');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var path = require('path');
-var libraryName = 'react-blazecss';
-var outputFile = libraryName + '.js';
+var path = require('path')
+var webpack = require('webpack')
+
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin
+
+var libraryName = 'react-blazecss'
 
 var config = {
   entry: __dirname + '/src/index.js',
   devtool: 'source-map',
   output: {
     path: __dirname + '/lib',
-    filename: outputFile,
+    filename: libraryName + '.min.js',
     library: libraryName,
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
   module: {
-    loaders: [ {
+    loaders: [{
       test: /\.js$/,
       loader: 'babel',
-      exclude: /(node_modules)/
+      exclude: /(node_modules)/,
+      query: {
+        presets: [
+          "es2015",
+          "react",
+          "stage-2"
+        ],
+        plugins: [
+          "transform-class-properties",
+          "transform-inline-environment-variables"
+        ]
+      }
     }]
   },
-  plugins: [
-    new UglifyJsPlugin({ minimize: true })
-  ],
   resolve: {
     root: path.resolve('./src'),
     extensions: ['', '.js']
   },
-  externals: [
-    'react'
-  ]
+  plugins: [
+    new UglifyJsPlugin({ minimize: true })
+  ],
+  externals: [{
+    'react': 'React'
+  }]
 };
 
 module.exports = config;
